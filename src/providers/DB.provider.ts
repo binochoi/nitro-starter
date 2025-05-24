44b37d5e0551ce052da390fc599e30795412e52f
bino <bonoself@gmail.com>
2025-05-24 14:16:18 +0900

    refactor:

--- src/providers/DB.provider.ts
+++ src/providers/DB.provider.ts
@@ -2,7 +2,7 @@ import postgres from 'postgres';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from '~/database/schema';
 
-export default (connection = process.env.DB_CONNECTION_STRING) => {
+export const DB = (connection = process.env.DB_CONNECTION_STRING) => {
     const client = postgres(connection || '');
     return drizzle(client, { schema });
 };
