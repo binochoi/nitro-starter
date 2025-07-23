1ad4b163c33a0a0cbb64a569a7209b7c3161a3b7
bino <bonoself@gmail.com>
2025-07-24 07:01:05 +0900

    .

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath } from "url";
 import cfBinding from "nitro-cloudflare-dev";
 
 //https://nitro.unjs.io/config
@@ -5,7 +6,7 @@ export default defineNitroConfig({
   compatibilityDate: "2025-03-05",
   modules: [cfBinding],
   srcDir: "src",
-  preset: 'cloudflare',
+  preset: 'cloudflare_module',
   minify: true,
   rollupConfig: {
     external: ['cloudflare:sockets'],
@@ -16,9 +17,16 @@ export default defineNitroConfig({
         strict: true,
         verbatimModuleSyntax: false,
         moduleResolution: 'bundler',
+        paths: {
+          server: [fileURLToPath(new URL("../admin/server", import.meta.url))],
+        },
       },
     }
   },
+  alias: {
+    server: fileURLToPath(new URL("../admin/server", import.meta.url)),
+  },
+
   logLevel: 4,
   // cloudflareDev: {
   //   configPath: './wrangler.toml',
--- src/providers/DB.provider.ts
+++ src/providers/DB.provider.ts
@@ -2,8 +2,9 @@ import postgres from 'postgres';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from '~/database/schema';
 
-export const DB = (connection = process.env.DB_CONNECTION_STRING) => {
-    const client = postgres(connection || '');
+export const DB = () => {
+    const config = useRuntimeConfig();
+    const client = postgres(config.dbConnectionString);
     return drizzle(client, { schema });
 };
 export { schema };
