1573d6bd1fd9a2f744ea7b485ecdd63235ad2a63
bino <bonoself@gmail.com>
2025-08-03 17:44:56 +0900

    refactor:

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "drizzle-kit";
 import { useBuildtimeConfig } from "server/config";
 
+const config = useBuildtimeConfig();
 export default defineConfig({
 	dialect: "postgresql",
 	schema: "./src/database/schema",
 	out: "./.cache/.migrations",
 	breakpoints: true,
 	dbCredentials: {
-		url: useBuildtimeConfig().db.connectionString,
+		url: config.db.connectionString,
 	},
 });
--- src/providers/DB.provider.ts
+++ src/providers/DB.provider.ts
@@ -1,6 +1,5 @@
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
-/** biome-ignore lint/performance/noNamespaceImport: reason */
 import * as schema from "server/database/schema";
 
 export const DB = () => {
